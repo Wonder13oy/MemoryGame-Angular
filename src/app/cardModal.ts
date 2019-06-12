30de26109422486408bcd99c5d9a04f97b22b4fc
Wonder13oy <wandilenxumalo131@gmail.com>
2019-06-12 14:55:29 +0200

    Added event listener

--- src/app/cardModal.ts
+++ src/app/cardModal.ts
@@ -1,4 +1,5 @@
 export class Card {
+  id: number;
   name: string;
   url: string;
 }
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from './../cardModal';
 import { CARDS } from './../cardPack';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-cards',
@@ -8,8 +10,25 @@ import { CARDS } from './../cardPack';
 })
 export class CardsComponent implements OnInit {
   cards = CARDS;
+  firstCard;
+  secondCard;
+  hasFlippedCard: boolean;
 
-  constructor() {}
+  constructor() {
+    this.hasFlippedCard = false;
+    const cardPack = document.querySelectorAll('.card-container');
+    cardPack.forEach((card) => card.addEventListener('click', this.flipCard));
+  }
 
   ngOnInit() {}
+
+  flipCard(): void {
+    this.firstCard.classList.toggle('flip');
+
+    setTimeout(() => {
+      this.checkMatch();
+    }, 1000);
+  }
+
+  checkMatch() {}
 }
