edcd964d4dbab240de3a7d90de55a99755099cc2
Wonder13oy <wandilenxumalo131@gmail.com>
2019-06-12 22:06:44 +0200

    Added addEventListener of all cards

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -12,12 +12,22 @@ export class CardsComponent implements OnInit {
   cards = CARDS;
   selectedCard: Card;
 
-  constructor() {}
+  constructor() {
+    document
+      .querySelectorAll('.card-container')
+      .forEach((card) => card.addEventListener('click', this.flipCard));
+  }
 
   ngOnInit() {}
 
-  flipCard(card: Card): void {
+  onSelect(card: Card): void {
     this.selectedCard = card;
+  }
+
+  flipCard(): void {
+    document
+      .querySelector(`#${this.selectedCard.id}`)
+      .removeEventListener('click', this.flipCard);
     console.log(this.selectedCard);
     // this.firstCard = setTimeout(() => {
     //   this.checkMatch();
