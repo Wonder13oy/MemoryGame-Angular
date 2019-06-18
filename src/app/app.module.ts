5b47a0b221f81b964fa183c23131d0c860ee276a
Wonder13oy <wandilenxumalo131@gmail.com>
2019-06-18 14:53:49 +0200

    Added time component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
 import { DisplayScreenComponent } from './display-screen/display-screen.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
-  declarations: [AppComponent, CardsComponent, DisplayScreenComponent],
+  declarations: [AppComponent, CardsComponent, DisplayScreenComponent, TimerComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
